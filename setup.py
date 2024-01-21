ceabaa1405473ffb975521b1fe0f652dbc0a2881
Divyansh Bisht <divyanshbisht989@gmail.com>
2024-01-21 21:29:26 +0530

    second commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from typing import List
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()     
+   
+
+__version__ = "0.0.1"   # make changes here
+REPO_NAME = "mongopkg"  # make changes here
+PKG_NAME= "packagemongo"  # make changes here
+AUTHOR_USER_NAME = "divyansh"
+AUTHOR_EMAIL = "divyanshbisht989@gmail.com"
+
+setup(
+    name=PKG_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A mongodb package for PYPI repo.",  # make changes here
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    )
